f789be5ef4dba8e12e5a9c4a5357b88fa1b8c594
skpatro <sunilpatro1985@gmail.com>
2023-04-25 19:33:47 +0530

    docker file with config

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -124,7 +124,7 @@ export const config: Options.Testrunner = {
         'goog:chromeOptions': {
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-            args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage']
+            //args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage'] //use this when headless is needed, for githubActions
             //https://chromedriver.chromium.org/capabilities
             //https://peter.sh/experiments/chromium-command-line-switches/
         },
@@ -182,7 +182,7 @@ export const config: Options.Testrunner = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [['selenium-standalone', {drivers:{chrome:'106.0.5249.61'}}]],
+    services: [['selenium-standalone', {drivers:{chrome:'112.0.5615.49'}}]],
     //services: [['selenium-standalone', {drivers: {chrome:'106.0.5249.61'}}]],
     //['selenium-standalone', { drivers: { firefox: '0.29.1', chrome: '98.0.4758.102', chromiumedge: 'latest' } }]
 
